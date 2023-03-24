edd2f9b5776dcdb5bd9032037f43e56b175f00aa
HWANGJunghoon <52022153+Mayor-of-District-Eunpyeong@users.noreply.github.com>
2023-03-24 16:01:11 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,79 @@
-import React from 'react';
-import AppNavigator from './AppNavigator';
+/*import AppNavigator from './AppNavigator';
 
 export default function App() {
   return <AppNavigator />;
-}
+}*/
 
-/* import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-const SignUpForm = () => {
+export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSignUp = () => {
-    // Handle sign-up logic here
+  const handleLogin = () => {
+    // 로그인 인증 구현 : Authentication
+  };
+
+  const handleSignup = () => {
+    // 회원가입 인증 구현
   };
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to aKoMpany</Text>
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
-        secureTextEntry
         value={password}
         onChangeText={setPassword}
+        secureTextEntry
       />
-      <Button title="Sign Up" onPress={handleSignUp} />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleSignup}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
-};
+}
 
-export default SignUpForm; */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
